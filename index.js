c203bcbce97023989d6b8241039f4bcae7141d26
Kehinde Balogun <balogunkehinde3@gmail.com>
2023-08-22 03:11:54 -0700

    modified the playGame function to stop after socre gets to 5

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const output = document.querySelector("#output")
-let computerScore = document.querySelector(".computerScore")
-let playerScore = document.querySelector(".playerScore")
+let displayComputerScore = document.querySelector(".computerScore")
+let displayPlayerScore = document.querySelector(".playerScore")
 const buttons = Array.from(document.querySelectorAll("button")) 
 let playerChoice;
+let playerScore = 0;
+let computerScore = 0
 let ChoicesArr = ['rock', 'paper', 'scissors']
 
-/* console.log(buttons); */
 
 function getComputerChoice() {
     let choice = Math.floor((Math.random() * 3) + 1)
@@ -14,58 +15,55 @@ function getComputerChoice() {
 
 function playRound(computer, player) {
     if (computer == "rock" && player == "rock"){
-        return "tie rock and rock"
+        output.innerText = "tie rock and rock"
+        return "tie"
      } else if (computer == "scissors" && player == "scissors"){
-        return "tie scissors and scissors"
+        output.innerText = "tie scissors and scissors"
+        return "tie"
      }  else if (computer == "paper" && player == "paper"){
-        return "tie paper and paper"
+        output.innerText = "tie paper and paper"
+        return "tie"
      } else if(computer == "rock" && player == "scissors"){
-        return "lose rock beat scissors"
+        output.innerText = "lose rock beat scissors"
+        return 'lose'
      } else if(computer == "scissors" && player == "rock"){
-        return "win rock beat scissors"
+        output.innerText = "win rock beat scissors"
+        return "win"
      } else if(computer == "paper" && player == "rock"){
-        return "lose paper beat rock"
+        output.innerText = "lose paper beat rock"
+        return 'lose'
      } else if(computer == "rock" && player == "paper"){
-        return "win paper beat rock"
+        output.innerText = "win paper beat rock"
+        return "win"
      } else if(computer == "paper" && player == "scissors"){
-        return "win scissors beat paper"
+        output.innerText = "win scissors beat paper"
+        return "win"
      } else if(computer == "scissors" && player == "paper"){
-        return "lose scissors beat paper"
-     } else {
-        return "lose you choose wrong"
+        output.innerText = "lose scissors beat paper"
+        return 'lose'
      }
 }
 
-
-
 function playGame() {
-   let score1 = 0;
-   let score2 = 0;
-
-   
-    //loop to call the playround 5 times
-    for(let i = 0; i < 5; i++) {
-        //for every iteration call plyRound and save the result
      let result = playRound(getComputerChoice(), playerChoice)
      console.log(result);
-     //increase the score based on the result
      if(result.includes("win")){
-        score1 += 1
+      displayPlayerScore.innerText = `${++playerScore}`;  
      } else if (result.includes("lose")){
-        score2 += 1
-     }
-
-     computerScore.innerText = `${score2}`;
-     playerScore.innerText = `${score1}`;
-
-    }
+      displayComputerScore.innerText = `${++computerScore}`;
+     }  
 }
 
 buttons.forEach((button) => {
    button.addEventListener('click', () => {
       playerChoice = button.className;
-      console.log(playerChoice);
-      playGame()
+      if(playerScore === 5){
+        alert("You Win Refresh to play again")
+      } else if(computerScore === 5) {
+         alert("You lose Refresh to play again")
+      } else {
+         playGame()
+      } 
    })
 })
 
@@ -77,4 +75,4 @@ buttons.forEach((button) => {
 
 
 
- 
+  
